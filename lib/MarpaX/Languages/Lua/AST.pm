48bb545b2bd4bd4cea5cf48a508a378f40864c01
rns <ruslan.shvedov@gmail.com>
2014-10-06 18:05:46 +0300

    imported lua test suite, start grammar conversion

--- lib/MarpaX/Languages/Lua/AST.pm
+++ lib/MarpaX/Languages/Lua/AST.pm
@@ -24,6 +24,73 @@ sub new {
 :default ::= action => [ name, value ]
 lexeme default = action => [ name, value ] latm => 1
 
+    # source: 8 – The Complete Syntax of Lua Lua 5.1 Reference Manual, http://www.lua.org/manual/5.1/manual.html
+    # * -- 0 or more: { ... }
+    # + -- 1 or more: [ ... ]
+#    chunk ::= {stat [';']} [laststat [';']]
+    chunk ::= {stat [';']} [laststat [';']]
+
+    block ::= chunk
+
+    stat ::=  varlist '=' explist |
+         functioncall |
+         <do> block <end> |
+         <while> exp <do> block <end> |
+         <repeat> block <until> exp |
+
+#         <if> exp <then> block {<elseif> exp <then> block} [<else> block] <end> |
+         <if> exp <then> block {<elseif> exp <then> block} [<else> block] <end> |
+
+#         <for> Name '=' exp ',' exp [',' exp] <do> block <end> |
+         <for> Name '=' exp ',' exp [',' exp] <do> block <end> |
+
+         <for> namelist <in> explist <do> block <end> |
+         <function> funcname funcbody |
+         <local> <function> Name funcbody |
+
+#         <local> namelist ['=' explist]
+         <local> namelist ['=' explist]
+
+    laststat ::= <return> [explist] | <break>
+
+    funcname ::= Name {'.' Name} [':' Name]
+
+    varlist ::= var {',' var}
+
+    var ::=  Name | prefixexp '[' exp ']' | prefixexp '.' Name
+
+    namelist ::= Name {',' Name}
+
+    explist ::= {exp ','} exp
+
+    exp ::=  <nil> | <false> | <true> | Number | String | '...' | function |
+         prefixexp | tableconstructor | exp binop exp | unop exp
+
+    prefixexp ::= var | functioncall | '(' exp ')'
+
+    functioncall ::=  prefixexp args | prefixexp ':' Name args
+
+    args ::=  '(' [explist] ')' | tableconstructor | String
+
+    function ::= <function> funcbody
+
+    funcbody ::= '(' [parlist] ')' block <end>
+
+    parlist ::= namelist [',' '...'] | '...'
+
+    tableconstructor ::= '{' [fieldlist] '}'
+
+    fieldlist ::= field {fieldsep field} [fieldsep]
+
+    field ::= '[' exp ']' '=' exp | Name '=' exp | exp
+
+    fieldsep ::= ',' | ';'
+
+    binop ::= '+' | '-' | '*' | '/' | '^' | '%' | '..' |
+         '<' | '<=' | '>' | '>=' | '==' | '~=' |
+         <and> | <or>
+
+    unop ::= '-' | <not> | '#'
 
 END_OF_SOURCE
         }
