c526d2545e18f18d38f1c0a9d55562281b93e45e
rns <ruslan.shvedov@gmail.com>
2014-10-08 12:19:33 +0300

    expected output files for testing against lua test suite

--- t/run_lua_tests.sh
+++ t/run_lua_tests.sh
@@ -11,29 +11,29 @@ cd lua5.1-tests
 export LUA_PATH="./?.lua;;"
 export LUA_INIT="package.path = '?;'..package.path"
 # lua all.lua fails at main.lua hence the tests are run individually
-lua api.lua
-lua attrib.lua
-lua big.lua
-lua calls.lua
-lua checktable.lua
-lua closure.lua
-lua code.lua
-lua constructs.lua
-lua db.lua
-lua errors.lua
-lua events.lua
-lua files.lua
-lua gc.lua
-lua literals.lua
-lua locals.lua
-lua main.lua
-lua math.lua
-lua nextvar.lua
-lua pm.lua
-lua sort.lua
-lua strings.lua
-lua vararg.lua
-lua verybig.lua
+lua api.lua > api.lua.out
+lua attrib.lua > attrib.lua.out
+lua big.lua > big.lua.out
+lua calls.lua > calls.lua.out
+lua checktable.lua > checktable.lua.out
+lua closure.lua > closure.lua.out
+lua code.lua > code.lua.out
+lua constructs.lua > constructs.lua.out
+lua db.lua > db.lua.out
+lua errors.lua > errors.lua.out
+lua events.lua > events.lua.out
+lua files.lua > files.lua.out
+lua gc.lua > gc.lua.out
+lua literals.lua > literals.lua.out
+lua locals.lua > locals.lua.out
+lua main.lua > main.lua.out
+lua math.lua > math.lua.out
+lua nextvar.lua > nextvar.lua.out
+lua pm.lua > pm.lua.out
+lua sort.lua > sort.lua.out
+lua strings.lua > strings.lua.out
+lua vararg.lua > vararg.lua.out
+lua verybig.lua > verybig.lua.out
 
 
 
