9bc5131b42271a167c311420c6cb078a1d4e11fd
rns <ruslan.shvedov@gmail.com>
2015-08-28 12:21:30 +0300

    add utility: t+

---
+++ bin/lua2ast.pl
@@ -0,0 +1,32 @@
+#!perl
+# Copyright 2015 Ruslan Shvedov
+
+# print lua ast on stdout
+
+use 5.010;
+use warnings;
+use strict;
+
+my ($fn) = @ARGV;
+
+do { say "Usage: $0 filespec"; exit 1 } unless $fn;
+
+#$fn = '../t/lua5.1-tests/api.lua';
+
+open my $fh, $fn or die "Can't open $fn: $@.";
+my $lua_src = do { local $/ = undef; <$fh> };
+close $fh;
+
+use MarpaX::Languages::Lua::AST;
+
+my $p = MarpaX::Languages::Lua::AST->new();
+
+use lib qw{/home/Ruslan/MarpaX-AST/lib};
+use lib qw{c:/cygwin/home/Ruslan/MarpaX-AST/lib};
+use MarpaX::AST;
+
+use Data::Dumper;
+my $ast = $p->parse( $lua_src );
+say Dumper( $p->parse( $lua_src ) );
+
+
